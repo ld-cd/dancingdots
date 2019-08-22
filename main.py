d103dcc5b0c9bcc27aa5136b1eef7d660bec830d
Aled Cuda <aledvirgil@gmail.com>
2019-08-21 18:37:51 -0700

    switch over to scene infra

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import pyglet, time, argparse
 
-import render, dot, configuration
+import render, dot, configuration, renderqueue, scene
 
 parse = argparse.ArgumentParser()
 parse.add_argument("configfile")
 args = parse.parse_args()
 
 config = configuration.read_config(configuration.open_config(args.configfile))
-dots = configuration.generate_dots(config)
-configuration.generate_tracks(config)
 
 conf = pyglet.gl.Config(sample_buffers=1, samples=4)
 window = pyglet.window.Window(width=config['window']['width'], height=config['window']['height'], config=conf)
 pyglet.gl.glClearColor(*config['window']['bgcolor'], 1)
 
-start_time = time.perf_counter()
+rqueue = renderqueue.RenderQueue(window)
+scene.display_scene(window, config, "fast", rqueue, True)
 
 def update(dt):
     pass
@@ -24,6 +23,6 @@ pyglet.clock.schedule_interval(update, 1/240)
 @window.event
 def on_draw():
     window.clear()
-    render.render_all(dot_list = dots, start = start_time)
+    rqueue.render()
 
 pyglet.app.run()
