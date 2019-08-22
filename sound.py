b4733a728ff9d3624eac859ea5c85f6eaaf7ac5e
Aled Cuda <aledvirgil@gmail.com>
2019-08-21 18:37:37 -0700

    make sound architecture more sane

--- sound.py
+++ sound.py
@@ -2,10 +2,7 @@ import pyglet, pydub, simpleaudio
 
 def make_player(track):
     sounds = sum([make_source(i) for i in track])
-    simpleaudio.play_buffer(sounds.raw_data, sounds.channels, sounds.sample_width, sounds.frame_rate)
-    pyglet.clock.schedule_interval(
-        lambda t: simpleaudio.play_buffer(sounds.raw_data, num_channels = sounds.channels, bytes_per_sample = sounds.sample_width, sample_rate = sounds.frame_rate)
-        , len(sounds)/1000)
+    return lambda t: simpleaudio.play_buffer(sounds.raw_data, sounds.channels, sounds.sample_width, sounds.frame_rate)
 
 def make_source(snippet):
     if snippet['type'] == "file":
